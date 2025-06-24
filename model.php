fa85f6c19b9ecab71a1c8b98d8a249f5c2a9446d
Rajo1903204 <r02243331@gmail.com>
2025-06-24 16:11:50 +0300

    lien connexion page

--- model.php
+++ model.php
@@ -4,11 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Entreprise</title>
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet" />
 </head>
 <body>
-    <p>Lien vers bootstrap</p>
+<div class="container mt-4">
+        <?php
+        include "inc/connexion.php";
+        include "inc/fonction.php";
+        if (isset($_GET['p'])) {
+            include "page/" . $_GET['p'] . ".php";
+        } else {
+            include "page/departements.php";
+        }
+        ?>
+    </div>
 </body>
 </html>
